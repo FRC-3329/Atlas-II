8a1fb41673e5d8d0c56d2d11b939ebb88481c9f5
Scaratech <entrpix@outlook.com>
2026-01-24 10:19:07 -0500

    fix: manually set swerve drive telemetry verbosity to high

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.cscore.CameraServerJNI.TelemetryKind;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
@@ -26,6 +27,8 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 
 import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 import swervelib.SwerveDrive;
 import swervelib.SwerveDriveTest;
 import swervelib.math.SwerveMath;
@@ -52,6 +55,8 @@ public class SwerveSubsystem extends SubsystemBase {
 	 * Sets up the drivetrain with our max speed, and configures feedforward values for accurate motor control.
 	 */
 	public SwerveSubsystem() {
+		SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
 		// Initialize the swerve drive from config files
 		try {
 			// SwerveParser reads all the JSON config files and creates a SwerveDrive object
