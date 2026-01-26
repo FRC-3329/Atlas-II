b01ccea6287c4e1a92761abab59619ea958a3fae
Scaratech <entrpix@outlook.com>
2026-01-26 17:39:31 -0500

    chore: update usage of deprecated code

--- src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
@@ -12,7 +12,7 @@ import frc.robot.utils.VisionData.EstimateConsumer;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.Matrix;
@@ -42,14 +42,14 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 	private final MutTime timeSinceLastUpdatedPose = Seconds.mutable(0.0);
 
 	private final StructPublisher<Pose2d> publisherRaw = NetworkTableInstance
-        .getDefault()
-		.getStructTopic("photonvision/WorldPoseRaw", Pose2d.struct)
-        .publish();
+			.getDefault()
+			.getStructTopic("photonvision/WorldPoseRaw", Pose2d.struct)
+			.publish();
 
 	private final StructPublisher<Pose2d> publisherFiltered = NetworkTableInstance
-        .getDefault()
-		.getStructTopic("photonvision/WorldPoseFiltered", Pose2d.struct)
-        .publish();
+			.getDefault()
+			.getStructTopic("photonvision/WorldPoseFiltered", Pose2d.struct)
+			.publish();
 
 	private Matrix<N3, N1> curStdDevs;
 	private Pose2d lastUpdatedPose;
@@ -71,22 +71,21 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 		cameraDisconnectedAlert = new Alert("photonvision", "PV Not Connected!!", AlertType.kError);
 		cameraNotTrackingAlert = new Alert("photonvision", "PV Not Tracking!!", AlertType.kWarning);
 
-        /*
-		    We will filter the pos data because we do not care about the phase delay added 
-            (the robot will be stationary when the pos is used).
-		    Also filtering because we could accidentally set out pos to a bad data pos if we are just grabbing the raw pos data.         
-        */  
+		/*
+		    We will filter the pos data because we do not care about the phase delay added
+		    (the robot will be stationary when the pos is used).
+
+		    Also filtering because we could accidentally set out pos to a bad data pos 
+            if we are just grabbing the raw pos data.
+		 */
 		xFilter = new MedianFilter(10);
 		yFilter = new MedianFilter(10);
 		thetaFilter = new MedianFilter(10);
 
 		photonEstimator = new PhotonPoseEstimator(
-            PVConstants.kTagLayout,
-            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-            PVConstants.ROBOT_TO_CAMERA
+				PVConstants.kTagLayout,
+				PVConstants.ROBOT_TO_CAMERA
         );
-
-		photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
 
 		lastUpdatedPose = new Pose2d(1, 1, Rotation2d.kZero);
 	}
@@ -97,19 +96,19 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 
 	/**
 	 * Calculates new std devs.
-     * This algorithm is a heuristic that creates dynamic standard deviations based on number of: 
-     *     - tags
-     *     - estimation strategy
-     *     - distance 
-     * from the tags.
-	 
+	 * This algorithm is a heuristic that creates dynamic standard deviations based
+	 * on number of:
+	 *     - tags
+	 *     - estimation strategy
+	 *     - distance
+	 * from the tags.
+	 * 
 	 * @param estimatedPose The estimated pose to guess standard deviations for.
 	 * @param targets       All targets in this camera frame
 	 */
 	private void updateEstimationStdDevs(
-        Optional<EstimatedRobotPose> estimatedPose,
-		List<PhotonTrackedTarget> targets
-    ) {
+			Optional<EstimatedRobotPose> estimatedPose,
+			List<PhotonTrackedTarget> targets) {
 		if (estimatedPose.isEmpty()) {
 			// No pose input, default to single-tag std devs
 			curStdDevs = PVConstants.kSingleTagStdDevs;
@@ -125,21 +124,21 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 				var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
 
 				if (tagPose.isEmpty()) {
-                    continue;
-                }
+					continue;
+				}
 
 				numTags++;
 
 				avgDist += tagPose
-					.get()
-					.toPose2d()
-					.getTranslation()
-					.getDistance(
-                        estimatedPose
-                            .get()
-                            .estimatedPose.toPose2d()
-                            .getTranslation()
-                    );
+						.get()
+						.toPose2d()
+						.getTranslation()
+						.getDistance(
+						    estimatedPose
+                                .get()
+                                .estimatedPose.toPose2d()
+							    .getTranslation()
+                        );
 			}
 
 			if (numTags == 0) {
@@ -152,14 +151,14 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 				// Decrease std devs if multiple targets are visible
 				if (numTags > 1) {
 					estStdDevs = PVConstants.kMultiTagStdDevs;
-                }
+				}
 
 				// Increase std devs based on (average) distance
 				if (numTags == 1 && avgDist > 4) {
 					estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-                } else {
+				} else {
 					estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
-                }
+				}
 
 				curStdDevs = estStdDevs;
 			}
@@ -167,9 +166,9 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 	}
 
 	/**
-	 * Returns the calculated world position of the robot. 
-     * This value is filtered. 
-     * Requires a tag to be seen within the last second to be valid.
+	 * Returns the calculated world position of the robot.
+	 * This value is filtered.
+	 * Requires a tag to be seen within the last second to be valid.
 	 * 
 	 * @return {@link Optional#of(Pose2d)} if a tag was seen within the last second,
 	 *         <p>
@@ -185,9 +184,11 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 	}
 
 	/**
-	 * Returns the latest standard deviations of the estimated pose from {@link #getEstimatedGlobalPose()},
-     * for use with {@link edu.wpi.first.math.estimator.SwerveDrivePoseEstimator SwerveDrivePoseEstimator}. 
-     * This should only be used when there are targets visible.
+	 * Returns the latest standard deviations of the estimated pose from
+	 * {@link #getEstimatedGlobalPose()},
+	 * for use with {@link edu.wpi.first.math.estimator.SwerveDrivePoseEstimator
+	 * SwerveDrivePoseEstimator}.
+	 * This should only be used when there are targets visible.
 	 */
 	public Matrix<N3, N1> getEstimationStdDevs() {
 		return curStdDevs;
@@ -206,7 +207,12 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 
 		Optional<EstimatedRobotPose> visionEst = Optional.empty();
 		for (var change : camera.getAllUnreadResults()) {
-			visionEst = photonEstimator.update(change);
+			// Attempt multi-tag first
+			visionEst = photonEstimator.estimateCoprocMultiTagPose(change);
+
+			if (visionEst.isEmpty()) {
+				visionEst = photonEstimator.estimateLowestAmbiguityPose(change);
+			}
 
 			if (useEstConsumer) {
 				updateEstimationStdDevs(visionEst, change.getTargets());
@@ -217,25 +223,24 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 
 				publisherRaw.accept(pose2d);
 
-			    if (useEstConsumer) {
+				if (useEstConsumer) {
 					var estStdDevs = getEstimationStdDevs();
 
 					estConsumer.accept(
-                        est.estimatedPose, est.timestampSeconds, estStdDevs
-                    );
+							est.estimatedPose, est.timestampSeconds, estStdDevs);
 				}
 
 				lastUpdatedPose = new Pose2d(
 					xFilter.calculate(pose2d.getX()),
 					yFilter.calculate(pose2d.getY()),
 					Rotation2d.fromRadians(
-                        thetaFilter.calculate(pose2d.getRotation().getRadians())
+						thetaFilter.calculate(pose2d.getRotation().getRadians())
                     )
                 );
-                
+
 				timeSinceLastUpdatedPose.mut_replace(PVConstants.VALID_TIME);
 				publisherFiltered.accept(lastUpdatedPose);
-		    });
+			});
 		}
 	}
 }
