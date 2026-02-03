6225fe4d5cb92f8d4c0342359e976e6238db672b
Scaratech <entrpix@outlook.com>
2026-02-03 16:59:32 -0500

    feat: update Flywheel constants

--- src/main/java/frc/robot/constants/FlywheelConstants.java
+++ src/main/java/frc/robot/constants/FlywheelConstants.java
@@ -2,23 +2,23 @@ package frc.robot.constants;
 
 import com.ctre.phoenix6.signals.InvertedValue;
 
-// TODO: Update values
 public final class FlywheelConstants {
     public static final class Flywheel {
         public static final int LEFT_ID = 2;
         public static final int RIGHT_ID = 1;
 
-        public static final double kP = 0.0;
+        public static final double kP = 0.10937;
         public static final double kI = 0.0;
         public static final double kD = 0.0;
-        public static final double kS = 0.0;
-        public static final double kV = 0.0;
-        public static final double kA = 0.0;
+        public static final double kS = 0.11085;
+        public static final double kV = 0.11419;
+        public static final double kA = 0.023967;
 
-        public static final double CURRENT_LIMIT = 40.0; // Amps
+        public static final double CURRENT_LIMIT = 45.0; // Amps
         public static final InvertedValue INVERTED = InvertedValue.CounterClockwise_Positive;
     }
 
+    // TODO: Populate hood constants
     public static final class Hood {
         public static final int HOOD_ID = 0;
 
