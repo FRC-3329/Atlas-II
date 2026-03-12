79f5c3c9f268e1e3a3cfb581d6407972cd307bfe
Scaratech <entrpix@outlook.com>
2026-03-12 07:28:35 -0400

    fix: Alert.AlertType.kWarning

--- src/main/java/frc/robot/subsystems/FlywheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlywheelSubsystem.java
@@ -27,6 +27,7 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.units.measure.MutAngularVelocity;
+import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -242,7 +243,7 @@ public class FlywheelSubsystem extends SubsystemBase {
     public void disableVaryingRPM() {
         varyingRPMEnabled = false;
         DogLog.log(getName() + "/VaryingRPMEnabled", false);
-        DogLog.logFault("VaryingRPMDisabled");
+        DogLog.logFault("VaryingRPMDisabled", Alert.AlertType.kWarning);
     }
 
     /**
